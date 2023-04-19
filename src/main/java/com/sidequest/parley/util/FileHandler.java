7e54fba60b509c81dcdd6b1264a6565a720cc0ca
dadgum82 <93083293+dadgum82@users.noreply.github.com>
2023-04-19 16:22:41 -0400

    Update FileHandler.java

--- src/main/java/com/sidequest/parley/util/FileHandler.java
+++ src/main/java/com/sidequest/parley/util/FileHandler.java
@@ -1,27 +1,21 @@
 package com.sidequest.parley.util;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVPrinter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
-import java.util.ArrayList;
-import javax.xml.bind.annotation.XmlType;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
-import com.sidequest.parley.model.User;
 
 public class FileHandler {
 	private final String DIRECTORY;
