091c48c9756c21fbca138262c8e613acf658d38e
dadgum82 <93083293+dadgum82@users.noreply.github.com>
2023-07-18 13:33:21 -0400

    fixed a bug with too much clean up

--- src/main/java/com/sidequest/parley/dao/ChatMessageDao.java
+++ src/main/java/com/sidequest/parley/dao/ChatMessageDao.java
@@ -7,5 +7,15 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface ChatMessageDao {
+    List<ChatMessage> getAllChatMessages(int id) throws SQLException;
+
+    void createChatMessage(ChatMessage chatMessage) throws SQLException;
+
+    void updateChatMessage(ChatMessage chatMessage) throws SQLException;
+
+    void deleteChatMessage(ChatMessage chatMessage) throws SQLException;
+
+    void dropChatMessageTable() throws SQLException;
+
     void createChatMessageTable() throws SQLException;
 }
